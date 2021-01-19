35498bbf31af04598b79cffd55b82809beb3ed7b
WwwwZzzzz <WwwwZzzzz@users.noreply.github.com>
2021-01-19 21:34:45 +0200

    website built with CSS

---
+++ website-minto/assets/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.banner-carousel').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+      });
+})
